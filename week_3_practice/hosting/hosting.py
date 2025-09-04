c2f0b198661c014c70b9cedeac0ed25d431bed25
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-09-04 17:14:30 +0530

    Update hosting.py

--- week_3_practice/hosting/hosting.py
+++ week_3_practice/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="week_3_practice/deployment",     # the local folder containing your files
-    repo_id="praneeth232/play-store-revenue-analysis",          # the target repo
+    repo_id="praneeth232/play-store-ad-revenue-prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
